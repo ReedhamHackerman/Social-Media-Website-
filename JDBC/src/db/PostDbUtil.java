7a9bf39a7825d8f3fd470fffcc8a3b6d4a18217a
ReedhamHackerman <Reedham1111997@gmail.com>
2020-08-07 19:57:49 -0400

    last Commmit

---
+++ JDBC/src/db/PostDbUtil.java
@@ -0,0 +1,19 @@
+package db;
+
+import javax.sql.DataSource;
+
+public class PostDbUtil 
+{
+	
+
+	private DataSource datasource;
+	
+	
+	public PostDbUtil(DataSource datasource) 
+	{
+		this.datasource = datasource;
+	}
+	
+	
+
+}
