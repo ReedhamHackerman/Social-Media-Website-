ec75dd5b3e1aef9f8bf70fca64124babff61b970
ReedhamHackerman <Reedham1111997@gmail.com>
2020-08-08 16:24:39 -0400

    Nothing to do

--- src/controller/CreateUser.java
+++ src/controller/CreateUser.java
@@ -19,7 +19,7 @@ public class CreateUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
    
-    public CreateUser() {
+    public CreateUser() {  
         super();
         // TODO Auto-generated constructor stub
     } 
