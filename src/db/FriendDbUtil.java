9aeee229ee34d8a1dd9f0eb61579f16c8256743a
Sai Koushik Reddy Mavillapally <smavilla@isimtl.local>
2020-08-20 13:38:53 -0400

    FRIENDS feature
    created frienddbutil and friendoperations

---
+++ src/db/FriendDbUtil.java
@@ -0,0 +1,139 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import model.Post;
+import model.User;
+
+public class FriendDbUtil 
+{
+	
+
+	private DataSource datasource;
+	
+	
+	
+	public FriendDbUtil(DataSource datasource) 
+	{
+		this.datasource = datasource;
+	}
+	
+	
+	public ArrayList<User> getAllFriendsOfUser(User email)  throws Exception
+	{
+		
+		Connection conn = null;
+		Statement stmt = null;
+		PreparedStatement pstmt = null;
+		ResultSet res = null;
+		ArrayList<User> allFriends =new ArrayList<User>();
+		
+		User emailid  = email;
+		try {
+			
+			conn =  this.datasource.getConnection();
+			
+			String sql = "select * from friends where emailid = ? ";
+			
+			pstmt = (PreparedStatement) conn.createStatement();
+			
+			res = pstmt.executeQuery(sql);
+		
+			
+			while(res.next()) {
+				
+				String emailId = res.getString("emailid");
+				
+				System.out.println(emailid);
+
+				allFriends.add(emailid);
+			}
+			
+			
+		} finally {
+			// TODO: handle finally clause
+			close(conn,stmt,pstmt,res);
+		}
+		return allFriends;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+public void UploadPost(Post post) throws Exception
+{
+       
+		Connection conn = null;
+		Statement stmt = null;
+		PreparedStatement pstmt = null;
+		ResultSet res = null;
+
+	
+		String emailid = post.getEmailId();
+		String content = post.getContent();
+		String date = post.getPostDate();
+		
+		
+		
+		try {
+			
+			conn =  this.datasource.getConnection();
+			
+			String sql = String.format("INSERT INTO posts (emailid,content,date) VALUES('%s','%s','%s')",emailid,content,date);
+			
+			stmt = conn.createStatement();
+			
+			stmt.executeUpdate(sql);
+			
+		} finally {
+			// TODO: handle finally clause
+			close(conn,stmt,pstmt,res);
+		}
+	}
+	
+	
+	
+	
+	
+	
+
+	private void close(Connection conn, Statement stmt, PreparedStatement pstmt, ResultSet res) {
+		
+		try {
+			
+			if(conn != null){				
+				conn.close();
+			}
+			
+			if(stmt != null) {				
+				stmt.close();
+			}
+			
+			if(pstmt != null) {				
+				pstmt.close();
+			}
+			
+			if(res != null) {				
+				res.close();
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}	
+	}
+
+}
